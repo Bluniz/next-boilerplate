e90ce49115b170a236f9b3d4daa8d5cee5325148
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-10-07 22:52:12 -0300

    Add Main component styles, content and new test

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,5 +1,15 @@
+import * as S from './styles'
+
 export const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="img/logo.svg" alt="React Avançado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="img/hero-illustration.svg"
+      alt="Um desenvolvedor de frete para tela com código"
+    />
+  </S.Wrapper>
 )
