6e981ae2c069e4f02b5b6e49b2bc817872834932
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-10-08 19:49:04 -0300

    Add plop for component generation

---
+++ generators/plop.js
@@ -0,0 +1,30 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        //! Para gerar nomes coloca as duas chaves pra gerar nomes dinâmicos
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.spec.tsx',
+        templateFile: 'templates/test.spec.tsx.hbs'
+      }
+    ]
+  })
+}
