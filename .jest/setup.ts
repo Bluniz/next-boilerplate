96ca6ac74c496de456c5eb9375e2e7ca6acf474a
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-10-19 11:45:30 -0300

    chore: change import of next-image.mock in setup.ts

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,4 +1,4 @@
 //! Necessário importar para habilitar as funcionalidades do jest-dom ao jest
 import "@testing-library/jest-dom"
 import "jest-styled-components"
-import "next-image.mock"
+import "./next-image.mock"
